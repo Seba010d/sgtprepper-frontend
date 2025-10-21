d0068d8349d9541ab2ac4b7285ccb7daf07b74e6
Seba010d <Seba010d@gmail.com>
2025-10-21 14:07:04 +0200

    Add HomePage component and atom view exports - made to 5:46 in the video

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,3 +1,9 @@
+import { Div, Heading, Paragraph } from "../views/atoms/index.js";
+
 export const HomePage = () => {
-    console.log('Hej verden')
-}
+  const div = Div();
+  const heading = Heading();
+  heading.innerHTML = "Hej verden";
+  div.append(heading);
+  return div;
+};
---
+++ js/views/atoms/index.js
@@ -0,0 +1,16 @@
+export const Div = () => {
+  const element = document.createElement("div");
+  return element;
+};
+
+export const Paragraph = () => {
+  const element = document.createElement("p");
+  return element;
+};
+
+export const Heading = (num = 1) => {
+  const element = document.createElement(`h${num}`);
+  return element;
+};
+
+// video - 5:46 minutes
