b0102cbb451229a76e34358ab1ded8528669aeef
Seba010d <Seba010d@gmail.com>
2025-10-23 14:14:58 +0200

    made the next video

--- js/controllers/CategoryController.js
+++ js/controllers/CategoryController.js
@@ -2,5 +2,12 @@ import { getList } from "../models/CategoryModel.js";
 
 export const getCategoryList = async () => {
   const data = await getList();
-  return data;
+
+  const formattedCategories = data.map((item) => ({
+    slug: item.slug,
+    title: item.title,
+    url: `/index.htm#products?category=${item.slug}`,
+  }));
+
+  return formattedCategories;
 };
